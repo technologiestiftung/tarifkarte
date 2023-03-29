678e07753bf997b6a5adcb32e0a56dee8abec224
hhack <hans.f.hack@googlemail.com>
2023-03-29 12:26:30 +0200

    useMatomo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useMatomo } from "@lib/hooks/useMatomo";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useMatomo();
+  return <Component {...pageProps} />;
 }
